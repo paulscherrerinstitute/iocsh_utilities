dc6e590d9c2ef415109471c880680a8d563fb0a0
Dirk Zimoch <dirk.zimoch@psi.ch>
2018-09-28 14:51:07 +0200

    EPICS_CA_MAX_ARRAY_BYTES was so huge that it caused out-of-memory problems

--- caFieldSize.c
+++ caFieldSize.c
@@ -2,5 +2,6 @@
 #include "db_convert.h"
 
 long caFieldSize(int type, long nelem) {
-    return dbr_size_n(dbf_type_to_DBR_CTRL(dbDBRnewToDBRold[type]), nelem);
+    /* round up native type as DBR_CTRL size to next multiple of 4 */
+    return (dbr_size_n(dbf_type_to_DBR_CTRL(dbDBRnewToDBRold[type]), nelem)+3)%4;
 }
